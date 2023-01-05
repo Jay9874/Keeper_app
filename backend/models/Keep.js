94d6b59c0af68b46b646c7f009f2d363cc53523d
Jay9874 <jayprakashsharma225@gmail.com>
2023-01-06 01:13:19 +0530

    delete file

--- backend/models/Keep.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const keepSchema = new mongoose.Schema({
-    title: String,
-    description: String
-})
-
-const Keep = new mongoose.model('Keep', keepSchema);
-module.exports = Keep;
